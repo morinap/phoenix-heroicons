54f328b6c959ad083bd7f90a5b394764d24908f5
Andrew Morin <amorin@clustertruck.com>
2022-07-14 15:08:50 -0400

    `mix new . --app phoenix_heroicons`

---
+++ lib/phoenix_heroicons.ex
@@ -0,0 +1,18 @@
+defmodule PhoenixHeroicons do
+  @moduledoc """
+  Documentation for `PhoenixHeroicons`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PhoenixHeroicons.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/phoenix_heroicons_test.exs
@@ -0,0 +1,8 @@
+defmodule PhoenixHeroiconsTest do
+  use ExUnit.Case
+  doctest PhoenixHeroicons
+
+  test "greets the world" do
+    assert PhoenixHeroicons.hello() == :world
+  end
+end
